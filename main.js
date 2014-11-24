f78bbd36aca5665aaf671ec054935345125b91e5
Giles Lavelle <giles.lavelle@gmail.com>
2014-11-24 17:41:57 +0000

    init

---
+++ main.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+var _ = require('underscore');
+
+var readCSV = function(path) {
+  return fs.readFileSync(path + '.csv', 'utf-8')
+    .split('\n')
+    .map(function(line) { return line.split(','); });
+};
+
+var writeCSV = function(path, data, header) {
+  var lines = [];
+
+  if (header) {
+    lines = lines.concat(header);
+  }
+
+  lines = lines.concat(data);
+
+  fs.writeFileSync(path + '.csv', lines.join('\n'));
+};
+
+var writeJSON = function(path, obj) {
+  if (_.isFunction(obj.toJSON)) {
+    obj = obj.toJSON();
+  }
+
+  if (_.isArray(obj) && _.isFunction(obj[0].toJSON)) {
+    obj = obj.map(function(item) { return item.toJSON(); });
+  }
+
+  fs.writeFileSync(path + '.json', JSON.stringify(obj));
+};
+
+var readJSON = function(path) {
+  return JSON.parse(fs.readFileSync(path + '.json', 'utf-8'));
+};
+
+var fatal = function(message) {
+  console.error(message.toString().red);
+  process.exit(1);
+};
+
+var pretty = function(object, indent) {
+  indent = indent || 2;
+  JSON.stringify(object, null, indent);
+};
+
+module.exports = {
+  writeJSON: writeJSON,
+  readJSON: readJSON,
+  readCSV: readCSV,
+  writeCSV: writeCSV,
+  fatal: fatal,
+  pretty: pretty
+};
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Gutils
+
+Functions I end up writing for every Node.js project.
+
+## Install
+
+```bash
+$ npm install --save gutils
+```
+
+## Use
+
+```js
+var util = require('gutils');
+```
+
+## License
+
+MIT
